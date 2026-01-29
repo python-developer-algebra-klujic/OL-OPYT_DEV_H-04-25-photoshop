ffdde5734e115ca92a7856f2bd5222a51b8401cc
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2026-01-29 18:31:10 +0100

    Update py_photoshop.py

--- py_photoshop.py
+++ py_photoshop.py
@@ -1,25 +1,27 @@
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageFilter
 
 
 
+MAX_WIDTH = 2000
 temp_image = None
 original_image = None
 def update_temp_image(updated_image: Image.Image):
     global temp_image
-    temp_image = updated_image
+    temp_image = updated_image.copy()
 
 def load_image(image_path='./images/Algebra_campus.jpg'):
     global original_image
 
     image = Image.open(image_path)
-    # TODO ako je slika veca od predefinirane sirine onda je smanji, inace ostavi
-    width = int(image.width / 2)
-    height = int(image.height / 2)
-    image = image.resize(size=(width, height))
+    if image.width >= MAX_WIDTH:
+        width = int(image.width / 2)
+        height = int(image.height / 2)
+        image = image.resize(size=(width, height))
+
     update_temp_image(image)
-    original_image = image
+    original_image = image.copy()
     return image
 
 def get_lbl_image(image=None):
@@ -30,14 +32,12 @@ def get_lbl_image(image=None):
         return ImageTk.PhotoImage(image=loaded_image)
 
 def get_image_data():
-    image = load_image()
-    return f'Image size:\t{image.size}\nImage format:\t{image.format}\nImage mode:\t{image.mode}'
-
+    return f'Image size:\t{temp_image.size}\nImage format:\t{temp_image.format}\nImage mode:\t{temp_image.mode}'
 
 
 
 def to_black_white():
-    image = temp_image
+    image = temp_image.copy()
     image = image.convert(mode='L')
     # sacuvaj ove promjene u tmp varijabli
     update_temp_image(image)
@@ -47,6 +47,38 @@ def to_black_white():
     lbl_display_photo.config(image=lbl_image)
     lbl_display_photo.image = lbl_image
 
+def flip_tb():
+    image = temp_image.copy()
+    image = image.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+    # sacuvaj ove promjene u tmp varijabli
+    update_temp_image(image)
+
+    # azurirj korisnicko sucelje (UI)
+    lbl_image = ImageTk.PhotoImage(image=image)
+    lbl_display_photo.config(image=lbl_image)
+    lbl_display_photo.image = lbl_image
+
+def flip_lr():
+    image = temp_image.copy()
+    image = image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+    # sacuvaj ove promjene u tmp varijabli
+    update_temp_image(image)
+
+    # azurirj korisnicko sucelje (UI)
+    lbl_image = ImageTk.PhotoImage(image=image)
+    lbl_display_photo.config(image=lbl_image)
+    lbl_display_photo.image = lbl_image
+
+def blur():
+    image = temp_image.copy()
+    image = image.filter(ImageFilter.GaussianBlur(radius=5))
+    # sacuvaj ove promjene u tmp varijabli
+    update_temp_image(image)
+
+    # azurirj korisnicko sucelje (UI)
+    lbl_image = ImageTk.PhotoImage(image=image)
+    lbl_display_photo.config(image=lbl_image)
+    lbl_display_photo.image = lbl_image
 
 
 
@@ -59,9 +91,11 @@ def open():
     lbl_image = get_lbl_image(image)
     lbl_display_photo.config(image=lbl_image)
     lbl_display_photo.image = lbl_image
+    lbl_image_data_var.set(value=get_image_data())
 
 def reset():
-    image = original_image
+    image = original_image.copy()
+    update_temp_image(original_image)
     lbl_image = get_lbl_image(image)
     lbl_display_photo.config(image=lbl_image)
     lbl_display_photo.image = lbl_image
@@ -84,13 +118,13 @@ lbl_display_photo.grid(column=0, row=0, sticky='NSEW')
 
 #endregion
 
+
 #region Actions Frame
 # Desni stupac s akcijama
 frm_actions = tk.Frame(root)
 frm_actions.grid(column=1, row=0, padx=10, pady=10, ipadx=5, ipady=5)
 
 
-# TODO Kod ucitavanja nove slike ovo se ne osvjezava, popraviti
 lbl_image_data_var = tk.StringVar(value=get_image_data())
 lbl_image_data = tk.Label(frm_actions,
                           text='Podaci o fotografiji',
@@ -119,8 +153,21 @@ btn_change_to_black_white = tk.Button(frm_actions,
                                       text='To Black \'n white',
                                       command=to_black_white)
 btn_change_to_black_white.grid(column=0, columnspan=3, row=2, padx=10, pady=10, ipadx=10, ipady=10)
+btn_flip_lr = tk.Button(frm_actions,
+                        text='Flip left right',
+                        command=flip_lr)
+btn_flip_lr.grid(column=0, row=3, padx=10, pady=10, ipadx=10, ipady=10)
+btn_flip_tb = tk.Button(frm_actions,
+                        text='Flip top bottom',
+                        command=flip_tb)
+btn_flip_tb.grid(column=2, row=3, padx=10, pady=10, ipadx=10, ipady=10)
+btn_blur = tk.Button(frm_actions,
+                        text='Blur',
+                        command=blur)
+btn_blur.grid(column=0, columnspan=3, row=4, padx=10, pady=10, ipadx=10, ipady=10)
 
 #endregion
+
 
 if __name__ == '__main__':
     root.mainloop()
